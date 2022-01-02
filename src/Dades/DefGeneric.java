4460089818c92a09d9cbfc84c4102adcf3e51d80
genismartinez <49500088-V@epp.urv.cat>
2022-01-02 16:18:41 +0100

    Primera version aoartados 9 y 10

--- src/Dades/DefGeneric.java
+++ src/Dades/DefGeneric.java
@@ -5,4 +5,31 @@ public interface DefGeneric <E> {
 	public int nElems();
 	public E consultatIessim(int i);
 	
+	// Apartat 9 -> Esborrar les dades d'una plantació
+	public int eliminar (int any) {
+		int elementos = nElems;
+		
+		for (int i=0;i<nElems;i++)
+			for(int j=0;j<llista[i].getNelems();i++)
+				if(llista[i].getPlantacions().getanyPlantacio().anyIgual(any))
+					llista[i]=null;
+				else
+					llista[i]=llista[i+1];
+	}
+	//Apartat 10 -> Modificar l'any de plantació d'una plantació
+	public int modificaAny (int any) {
+		int elementos = nElems;
+		
+		for(int i=0;i<nElems;i++)
+			for(int j=0;llista[i].getNelems();j++)
+				
+	}
+	
+	public boolean anyIgual(int any) {
+		boolean esIgual;
+		if(anyPlantacio==any)
+			esIgual=true;
+			else
+			esIgual=false;
+	}
 }
