4fc92330777ec881b93fdfb345ef4940c511b865
JoanICG <joanignasiguardia@gmail.com>
2022-01-07 19:33:32 +0100

    Classe per a la interficie grafica començat
    Copia se seguretat solament xd

--- src/InterficieGrafica/Interficie.java
+++ src/InterficieGrafica/Interficie.java
@@ -1,5 +1,55 @@
 package InterficieGrafica;
 
-public class Interficie {
-//Funciona?
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Interficie extends JFrame {
+//Funciona? 
+	public Interficie() {
+		//Establir tamany de pantalla
+		this.setSize(500,500);
+		
+		//Li donem un nom a la finestra
+		setTitle("Prac 3");
+		
+	
+		
+		//Fem que la finestra aparegui a un lloc determinat
+		//setLocation(100,200);
+		
+		//Establim un tamany i una posicio de la finestra, (x, y,ampadax, llargaday)
+		//setBounds(100, 200, 500, 500);
+		
+		//Posem la fienstra al centre de la pantalla
+		setLocationRelativeTo(null);
+		
+		iniciarComponents();
+		
+		//Quan tanquis la finestra finalitza TOT el programa
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	private void iniciarComponents() {
+		JPanel panel = new JPanel();
+		
+		//Afegir el panel que acabem de crear
+		this.getContentPane().add(panel);
+		
+		//Desactivem el diseny per a poder posar la etiqueta on vulguem
+		panel.setLayout(null);
+		
+		//Crear una etiqueta de text
+		JLabel etiqueta = new JLabel();
+		
+		//Establim el text de la etiqueta
+		etiqueta.setText("Hola");
+		
+		//Modificar la posicio de la etiqueta i el tamany (x, y,ampadax, llargaday)
+		etiqueta.setBounds(100, 100, 50, 30);
+		
+		//Agregar la etiqueta
+		panel.add(etiqueta);
+	}
 }
+ 
---
+++ src/InterficieGrafica/ProgramaEnInterficie.java
@@ -0,0 +1,14 @@
+package InterficieGrafica;
+
+public class ProgramaEnInterficie {
+
+	public static void main(String[] args) {
+		Interficie Finestra = new Interficie();
+		
+		Finestra.setVisible(true);
+		
+		
+	}
+
+}
+
